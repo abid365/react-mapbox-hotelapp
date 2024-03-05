f30543fea4942abe3450f941f7156f9b16fac846
abid365 <abidibnazam@gmail.com>
2024-03-06 01:13:25 +0600

    sidebar completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import SideNav from "./Layouts/SideNav";
 
 function App() {
   return (
-    <div className="flex">
-      <div>
+    <div className="grid lg:grid-cols-12 border border-red-400">
+      <div className="border-2 border-emerald-400 w-fit h-screen">
         <SideNav />
       </div>
-      <Outlet />
-      <div>
+      <div className="col-span-7 scroll-auto w-100 h-auto">
+        <Outlet />
+      </div>
+      <div className="border-2 border-indigo-400 col-span-4 h-screen">
         <Map />
       </div>
     </div>
--- src/Layouts/SideNav.jsx
+++ src/Layouts/SideNav.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { RiHome6Line, RiCheckboxMultipleLine } from "react-icons/ri";
+import { HiOutlineChartSquareBar } from "react-icons/hi";
+import { RxLayers } from "react-icons/rx";
+import { PiFlagBannerBold } from "react-icons/pi";
+import { FiUsers } from "react-icons/fi";
+import { LiaLifeRing } from "react-icons/lia";
+import { FiSettings } from "react-icons/fi";
+import { Avatar } from "@radix-ui/react-avatar";
+import { AvatarImage } from "@radix-ui/react-avatar";
+import { AvatarFallback } from "@radix-ui/react-avatar";
 
 const SideNav = () => {
-  return <div>This is sidenav</div>;
+  return (
+    <div className="pt-10 flex flex-col justify-between items-center min-h-[100vh]">
+      {/* upper navigations */}
+      <div className="px-3 flex flex-col items-center gap-y-6">
+        <RiHome6Line className="text-2xl" />
+        <HiOutlineChartSquareBar className="text-2xl" />
+        <RxLayers className="text-2xl" />
+        <RiCheckboxMultipleLine className="text-2xl" />
+        <PiFlagBannerBold className="text-2xl" />
+        <FiUsers className="text-2xl" />
+      </div>
+      {/* loweer navigations */}
+      <div className="px-3 flex flex-col items-center gap-y-5 mt-auto pb-4">
+        <LiaLifeRing className="text-2xl" />
+        <FiSettings className="text-xl" />
+
+        {/* avatar */}
+        <Avatar className="h-10 w-10 border rounded-full">
+          <AvatarImage
+            className="border rounded-full"
+            src="https://github.com/shadcn.png"
+          />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </div>
+    </div>
+  );
 };
 
 export default SideNav;
